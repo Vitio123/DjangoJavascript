d89c505b2158d8cc6e83411a6d05309a47f96686
Vitio <quijanoangel08@gmail.com>
2025-03-03 12:43:36 -0500

    Nuevo cambio al frontend

--- frontend/src/components/Create.jsx
+++ frontend/src/components/Create.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { Box, Typography } from "@mui/material";
 
 export default function Create() {
-  return <div>Create</div>;
+  return (
+    <div>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          background: "#00003f",
+          marginBottom: "10px",
+        }}
+      >
+        <Typography sx={{ marginLeft: "20px" }}>Create</Typography>
+      </Box>
+    </div>
+  );
 }
--- frontend/src/components/form/MyDatePickerField.jsx
+++ frontend/src/components/form/MyDatePickerField.jsx
@@ -4,7 +4,6 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { Controller } from "react-hook-form";
 
-// eslint-disable-next-line react/prop-types
 export default function MyDatePickerField({ label, control, name }) {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
